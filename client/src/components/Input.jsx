3d31cf0beeba385162835466548034fb0780e8ac
kakotyanimesh <phb21013@tezu.ac.in>
2024-10-25 00:26:03 +0530

    minor change

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({type, placeholder, icon}) => {
   return (
     <div className='relative '>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3'>{icon}</div>
-        <input type={type} placeholder={placeholder} className='sm:w-64 pl-10 py-1 rounded' />
+        <input type={type} placeholder={placeholder} className='w-64 pl-10 py-1 rounded' />
     </div>
   )
 }
